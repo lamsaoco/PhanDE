aed7046e4a5eaa2bd0deb07e008d27faafce32c4
lamsaoco <50307604+lamsaoco@users.noreply.github.com>
2025-08-22 18:44:03 +0000

    commit

--- ingest_data.py
+++ ingest_data.py
@@ -19,8 +19,14 @@ def main(params):
     port = params.port
     db = params.db
     table_name = params.table_name
+    url = params.url
 
-    csv_name = 'yellow_tripdata_2021-01.csv'
+    if url.endswith('.csv.gz'):
+        csv_name = 'output.csv.gz'
+    else:
+        csv_name = 'output.csv'
+
+    os.system(f"wget {url} -O {csv_name}")
 
     engine = create_engine(f'postgresql://{user}:{password}@{host}:{port}/{db}')
     df_iter = pd.read_csv(csv_name, iterator=True, chunksize=10000)
@@ -54,13 +60,7 @@ if __name__ == '__main__':
     parser.add_argument('--port', type=int, help='port for postgres')
     parser.add_argument('--db', help='database name for postgres')
     parser.add_argument('--table_name', help='name of the table where we will write the results to')
+    parser.add_argument('--url', required=True, help='url of the csv file')
 
     args = parser.parse_args()
     main(args)
-
-
-
-
-
-
-
